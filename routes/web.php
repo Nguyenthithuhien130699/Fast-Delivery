1bdc7bc95e64090a78a8b3ba75a09c6c0bf9d865
= <=>
2020-10-24 16:36:16 +0700

    add,edit district

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,6 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Backend', 'middleware' => ['a
         Route::post('update/{id}', 'UserController@postUpdate')->name('admin.user.postUpdate');
         Route::post('update-password/{id}', 'UserController@updatePassword')->name('admin.user.updatePassword');
         Route::post('update-avatar/{id}', 'UserController@updateAvatar')->name('admin.user.updateAvatar');
-      
     });
     Route::group(['prefix' => 'province'], function () {
         Route::get('/', 'ProvinceController@index')->name('admin.province.index');
@@ -47,6 +46,18 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Backend', 'middleware' => ['a
         Route::get('delete/{id}', 'ProvinceController@delete')->name('admin.province.delete');
         Route::post('update/{id}', 'ProvinceController@update')->name('admin.province.update');
 
- 
-});
+        Route::get('district', 'ProvinceController@district')->name('admin.province.district');
+        Route::get('district-edit/{id}', 'ProvinceController@districtEdit')->name('admin.district.edit');
+        Route::get('district-delete/{id}', 'ProvinceController@districtDelete')->name('admin.district.delete');
+
+        Route::get('ward', 'ProvinceController@ward')->name('admin.province.ward');
+        Route::get('ward-edit/{id}', 'ProvinceController@wardEdit')->name('admin.ward.edit');
+        Route::get('ward-delete/{id}', 'ProvinceController@wardDelete')->name('admin.ward.delete');
+
+        Route::post('district-create', 'ProvinceController@districtCreate')->name('admin.district.create');
+        Route::post('ward-create', 'ProvinceController@wardCreate')->name('admin.ward.create');
+
+        Route::post('district-update/{id}', 'ProvinceController@districtUpdate')->name('admin.district.update');
+        Route::post('ward-update/{id}', 'ProvinceController@wardUpdate')->name('admin.ward.update');
+    });
 });
